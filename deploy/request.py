b225e1aba65b20ca1a1ee829fa78497de7979065
ProsperChuks <prosperc40@gmail.com>
2021-10-10 22:13:10 +0100

    ...

--- deploy/request.py
+++ deploy/request.py
@@ -1,7 +1,7 @@
 import requests
 
-url = 'http://localhost:5000/results'
-r = requests.post(url,json={'fixed acidity':7, 'volatile acidity':0.34,
+url = 'https://wine-328608.uc.r.appspot.com/results'
+r = requests.post(url, json={'fixed acidity':7, 'volatile acidity':0.34,
      'citric acid':0.48, 'chlorides':0.11, 'free sulfur dioxide': 12,
      'total sulfur dioxide':39, 'density':1, 'pH':3,
      'sulphates':1.26, 'alcohol':10.9})
